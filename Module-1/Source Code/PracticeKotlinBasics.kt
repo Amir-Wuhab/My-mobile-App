ea79f1cb0f7c589b1152df6acc54616f1960a5cb
My PC <amirowehab@gmail.com>
2026-09-18 23:00:30 +0800

    Add module-1

--- Module-1/Source Code/PracticeKotlinBasics.kt
+++ Module-1/Source Code/PracticeKotlinBasics.kt
@@ -1,27 +1,26 @@
 fun main() {
-    printNotificationMessages()
-    fixCompileErrors()
-    demonstrateStringTemplates()
-    formatMessages("Notification System", 5)
-    performMathOperations()
-    printEmailDetails(sender = "support@android.com", recipient = "user@example.com")
-    printEmailDetails(sender = "alerts@android.com", recipient = "user@example.com", subject = "Security Alert")
-    refactorDuplicateCode()
+    exercisePrintMessages()
+    exerciseFixCompileError()
+    exerciseStringTemplates()
+    exerciseStringConcatenation()
+    exerciseMathOperations()
+    exerciseDefaultParameters()
+    exerciseRefactorDuplicateCode()
 }
 
-fun printNotificationMessages() {
+fun exercisePrintMessages() {
     println("Use the val keyword when the value doesn't change.")
     println("Use the var keyword when the value can change.")
     println("When a program runs, the variables are updated as needed.")
 }
 
-fun fixCompileErrors() {
+fun exerciseFixCompileError() {
     val city = "New York"
     val temperature = 22
     println("The temperature in $city is $temperature degrees Celsius.")
 }
 
-fun demonstrateStringTemplates() {
+fun exerciseStringTemplates() {
     val numberOfPhotos = 100
     val numberOfDeletedPhotos = 10
     println("$numberOfPhotos photos in total.")
@@ -29,37 +28,37 @@ fun demonstrateStringTemplates() {
     println("${numberOfPhotos - numberOfDeletedPhotos} photos remaining.")
 }
 
-fun formatMessages(systemName: String, alertCount: Int) {
-    val formattedMessage = "[$systemName]: $alertCount urgent notifications waiting."
-    println(formattedMessage)
+fun exerciseStringConcatenation() {
+    val numberOfNotifications = 5
+    val notificationPrefix = "You have "
+    val notificationSuffix = " new notifications."
+    println(notificationPrefix + numberOfNotifications + notificationSuffix)
 }
 
-fun performMathOperations() {
+fun exerciseMathOperations() {
     val firstNumber = 10
     val secondNumber = 5
     val thirdNumber = 8
-    
     val result = addNumbers(firstNumber, secondNumber)
     val anotherResult = addNumbers(firstNumber, thirdNumber)
-    
     println("$firstNumber + $secondNumber = $result")
     println("$firstNumber + $thirdNumber = $anotherResult")
-    println("$firstNumber * $secondNumber = ${multiplyNumbers(firstNumber, secondNumber)}")
 }
 
 fun addNumbers(first: Int, second: Int): Int {
     return first + second
 }
 
-fun multiplyNumbers(first: Int, second: Int): Int {
-    return first * second
+fun exerciseDefaultParameters() {
+    displayEmailDetails(sender = "support@android.com", recipient = "user@example.com")
+    displayEmailDetails(sender = "alerts@android.com", recipient = "user@example.com", subject = "Security Alert")
 }
 
-fun printEmailDetails(sender: String, recipient: String, subject: String = "No Subject") {
+fun displayEmailDetails(sender: String, recipient: String, subject: String = "No Subject") {
     println("Sender: $sender | Recipient: $recipient | Subject: $subject")
 }
 
-fun refactorDuplicateCode() {
+fun exerciseRefactorDuplicateCode() {
     printWeatherReport("City A", 20, 25, 80)
     printWeatherReport("City B", 15, 18, 65)
     printWeatherReport("City C", 30, 35, 40)
